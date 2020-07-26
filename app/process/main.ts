ef7bb24ecb8fde3a03a4822261a94250964de83c
happysmilecode <kevinharuto430@gmail.com>
2020-07-26 19:40:59 +0200

    :art: refine code structure

--- app/process/main.ts
+++ app/process/main.ts
@@ -9,9 +9,6 @@ function createWindow() {
 			nodeIntegration: false,
 			nodeIntegrationInWorker: false
 		},
-		options: {
-			fullscreen: true
-		},
 		width: 1040,
 	});
 
@@ -22,23 +19,12 @@ function createWindow() {
 	mainWindow.webContents.openDevTools();
 }
 
-
-// Cette méthode sera appelée quant Electron aura fini
-// de s'initialiser et prêt à créer des fenêtres de navigation.
-// Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
 app.whenReady().then(createWindow)
 
-// Quitter si toutes les fenêtres ont été fermées.
 app.on('window-all-closed', () => {
-	// Sur macOS, il est commun pour une application et leur barre de menu
-	// de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
+	if (process.platform !== 'darwin') app.quit();
 })
 
 app.on('activate', () => {
-	// Sur macOS, il est commun de re-créer une fenêtre de l'application quand
-	// l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
 	if (BrowserWindow.getAllWindows().length === 0) createWindow();
 })
